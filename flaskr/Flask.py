abdeb0971e3cb216be236770e8d7e8ed0892dd5b
Eduardo <eduardo.olvera02@utrgv.edu>
2024-11-11 19:15:58 -0600

    flask-v2

--- flaskr/Flask.py
+++ flaskr/Flask.py
@@ -1,11 +1,31 @@
 from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
-a=False
 
+@app.route("/Main/")
 @app.route("/")
 def home():
-	return render_template("Main.html", title="login")
+	return render_template("Main.html")
+
+@app.route("/Login")
+def login():
+	return render_template("Login.html")
+
+@app.route("/Contact")
+def contact():
+	return render_template("Contact_Us.html")
+
+@app.route("/Forgot")
+def forgot():
+	return render_template("Forgot.html")
+
+@app.route("/Register")
+def register():
+	return render_template("Register.html")
+
+@app.route("/Cart")
+def cart():
+	return render_template("Cart.html")
 
 # @app.route("/<name>")
 # def user(name):
